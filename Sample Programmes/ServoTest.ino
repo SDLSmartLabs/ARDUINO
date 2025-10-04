009c7a92e0fe7d1c8d218b1b2d3c8238a46b470c
Shanwill Pinto <62848565+data-charya@users.noreply.github.com>
2025-10-04 08:29:23 +0200

    Add ServoTest program to control servo motor movement (#73)

---
+++ Sample Programmes/ServoTest.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo myServo;
+int pos = 0;
+
+void setup() {
+  myServo.attach(9); // PWM pin
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos++) {
+    myServo.write(pos);
+    delay(15);
+  }
+  for (pos = 180; pos >= 0; pos--) {
+    myServo.write(pos);
+    delay(15);
+  }
+}
